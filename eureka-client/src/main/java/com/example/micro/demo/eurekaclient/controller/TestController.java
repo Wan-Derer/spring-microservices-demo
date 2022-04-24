f7a35b68b5844fb2b31fa7fb08bb2e381910893d
Wan-Derer <frd-ru@yandex.ru>
2022-04-25 00:10:35 +0300

    API gateway created

--- eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.micro.demo.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestClient {
+@RequestMapping("/main")
+public class TestController {
 
   @GetMapping("/test")
   public String test(){
@@ -16,3 +18,5 @@ public class TestClient {
 
 
 }
+
+
