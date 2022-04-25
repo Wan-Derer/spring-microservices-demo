fae84eb933651faaa97954cf9491b6aca864f99a
Wan-Derer <frd-ru@yandex.ru>
2022-04-25 10:38:27 +0300

    Second microservice created

---
+++ eureka-client2/src/main/java/com/example/micro/demo/eurekaclient2/controller/Client2Controller.java
@@ -0,0 +1,22 @@
+package com.example.micro.demo.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class Client2Controller {
+
+  @GetMapping("/name")
+  public String name(){
+    return "Привет, МИР!";
+  }
+
+
+
+
+
+}
+
+
