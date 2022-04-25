2bccbcf524f2f37a26606480d711e79a9214dc4e
Wan-Derer <frd-ru@yandex.ru>
2022-04-25 12:11:17 +0300

    Many Client Instances configuration

--- eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+  @Value("${eureka.instance.instance-id}")
+  private String id;
+
+
   @GetMapping("/test")
   public String test(){
-    return "Test Тест";
+    return "Test Тест " + id;
   }
 
 
